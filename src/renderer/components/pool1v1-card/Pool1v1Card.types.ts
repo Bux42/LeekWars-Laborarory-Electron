029801a489a52fd786589e62137648716270837b
Bux42 <roszwoka2@gmail.com>
2026-01-08 23:31:13 +0100

    Remove obsolete Pool1v1Card component

--- src/renderer/components/pool1v1-card/Pool1v1Card.types.ts
+++
@@ -1,8 +0,0 @@
-import { IPoolOneVersusOneResponse } from '../../../services/leekwars-laboratory/pools/PoolOneVersusOne.types';
-import { ILeek } from '../../../services/leekwars-laboratory/types/leek/Leek.types';
-
-export interface IPool1v1CardProps {
-  pool: IPoolOneVersusOneResponse;
-  availableLeeks: ILeek[];
-  onPoolUpdate: () => void;
-}
