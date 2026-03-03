17bdb4c7080caaf0aabaa897d1a097aefcde3138
Bux42 <roszwoka2@gmail.com>
2026-03-03 10:24:23 +0100

    Use leek image component inn PoolDuelLeek

--- src/renderer/components/pool/duel/pool-duel-leek/PoolDuelLeek.tsx
+++ src/renderer/components/pool/duel/pool-duel-leek/PoolDuelLeek.tsx
@@ -1,15 +1,12 @@
 import { getImage } from '../../../../utils/ImageLoader';
 import { IPoolDuelLeekProps } from './PoolDuelLeek.types';
 import { poolDuelLeekStyles } from './PoolDuelLeek.styles';
+import LeekImage from '../../../leek/leek-image/LeekImage';
 
 function PoolDuelLeek({ leek }: IPoolDuelLeekProps) {
   return (
     <div style={poolDuelLeekStyles.container}>
-      <img
-        src={getImage(`leekwars/image/leek/${leek.imageName}`)}
-        alt={leek.name}
-        style={poolDuelLeekStyles.image}
-      />
+      <LeekImage leek={leek} showTooltip height={40} width={40} />
       <div style={poolDuelLeekStyles.details}>
         <div style={poolDuelLeekStyles.name}>{leek.name}</div>
         <div>Level {leek.build.level}</div>
